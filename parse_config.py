3da54cb81db2af40bcb48c100d975fa3ffb46b59
fffffarmer <fffffarmer@sjtu.edu.cn>
2020-12-29 17:02:56 +0800

    update config

--- parse_config.py
+++ parse_config.py
@@ -4,9 +4,9 @@ from pathlib import Path
 from functools import reduce, partial
 from operator import getitem
 from datetime import datetime
-from logger import setup_logging
-from utils import read_json, write_json
 
+from logger import setup_logging
+from utils.common import read_json, write_json
 
 class ConfigParser:
     def __init__(self, config, resume=None, modification=None, run_id=None):
@@ -18,29 +18,29 @@ class ConfigParser:
         :param modification: Dict keychain:value, specifying position values to be replaced from config dict.
         :param run_id: Unique Identifier for training processes. Used to save checkpoints and training log. Timestamp is being used as default
         """
-        # load config file and apply modification
+        # load config file and apply modification.
         self._config = _update_config(config, modification)
         self.resume = resume
 
-        # set save_dir where trained model and log will be saved.
-        save_dir = Path(self.config['trainer']['save_dir'])
+        # Let save_dir where trained model and log will be saved.
+        save_dirname = Path(self.config["trainer"]["save_dirname"])
 
-        exper_name = self.config['name']
-        if run_id is None: # use timestamp as default run-id
-            run_id = datetime.now().strftime(r'%m%d_%H%M%S')
-        self._save_dir = save_dir / 'models' / exper_name / run_id
-        self._log_dir = save_dir / 'log' / exper_name / run_id
+        exper_name = self.config["name"]
+        if run_id is None: # use timestamp as default run-id.
+            run_id = datetime.now().strftime(r"%m%d_%H%M%S")
+        self._save_dirname = save_dirname / "models" / exper_name / run_id
+        self._log_dirname = save_dirname / "log" / exper_name / run_id
 
-        # make directory for saving checkpoints and log.
-        exist_ok = run_id == ''
-        self.save_dir.mkdir(parents=True, exist_ok=exist_ok)
-        self.log_dir.mkdir(parents=True, exist_ok=exist_ok)
+        # Make directory for saving checkpoints and log.
+        exist_ok = run_id == ""
+        self.save_dirname.mkdir(parents=True, exist_ok=exist_ok)
+        self.log_dirname.mkdir(parents=True, exist_ok=exist_ok)
 
-        # save updated config file to the checkpoint dir
-        write_json(self.config, self.save_dir / 'config.json')
+        # Save updated config file to the checkpoint dir.
+        write_json(self.config, self.save_dirname / "config.json")
 
-        # configure logging module
-        setup_logging(self.log_dir)
+        # Configure logging module
+        setup_logging(self.log_dirname)
         self.log_levels = {
             0: logging.WARNING,
             1: logging.INFO,
@@ -48,7 +48,7 @@ class ConfigParser:
         }
 
     @classmethod
-    def from_args(cls, args, options=''):
+    def from_args(cls, args, options=""):
         """
         Initialize this class from some cli arguments. Used in train, test.
         """
@@ -61,19 +61,19 @@ class ConfigParser:
             os.environ["CUDA_VISIBLE_DEVICES"] = args.device
         if args.resume is not None:
             resume = Path(args.resume)
-            cfg_fname = resume.parent / 'config.json'
+            cfg_fname = resume.parent / "config.json"
         else:
             msg_no_cfg = "Configuration file need to be specified. Add '-c config.json', for example."
             assert args.config is not None, msg_no_cfg
             resume = None
             cfg_fname = Path(args.config)
-        
+
         config = read_json(cfg_fname)
         if args.config and resume:
-            # update new config for fine-tuning
+            # Update new config for fine-tuning.
             config.update(read_json(args.config))
 
-        # parse custom cli options into dictionary
+        # Parse custom cli options into dictionary.
         modification = {opt.target : getattr(args, _get_opt_name(opt.flags)) for opt in options}
         return cls(config, resume, modification)
 
@@ -86,9 +86,9 @@ class ConfigParser:
         is equivalent to
         `object = module.name(a, b=1)`
         """
-        module_name = self[name]['type']
-        module_args = dict(self[name]['args'])
-        assert all([k not in module_args for k in kwargs]), 'Overwriting kwargs given in config file is not allowed'
+        module_name = self[name]["type"]
+        module_args = dict(self[name]["args"])
+        assert all([k not in module_args for k in kwargs]), "Overwriting kwargs given in config file is not allowed"
         module_args.update(kwargs)
         return getattr(module, module_name)(*args, **module_args)
 
@@ -101,37 +101,35 @@ class ConfigParser:
         is equivalent to
         `function = lambda *args, **kwargs: module.name(a, *args, b=1, **kwargs)`.
         """
-        module_name = self[name]['type']
-        module_args = dict(self[name]['args'])
-        assert all([k not in module_args for k in kwargs]), 'Overwriting kwargs given in config file is not allowed'
+        module_name = self[name]["type"]
+        module_args = dict(self[name]["args"])
+        assert all([k not in module_args for k in kwargs]), "Overwriting kwargs given in config file is not allowed"
         module_args.update(kwargs)
         return partial(getattr(module, module_name), *args, **module_args)
 
     def __getitem__(self, name):
-        """Access items like ordinary dict."""
+        """ Access items like ordinary dict."""
         return self.config[name]
 
     def get_logger(self, name, verbosity=2):
-        msg_verbosity = 'verbosity option {} is invalid. Valid options are {}.'.format(verbosity, self.log_levels.keys())
+        msg_verbosity = "verbosity option {} is invalid. Valid options are {}.".format(verbosity, self.log_levels.keys())
         assert verbosity in self.log_levels, msg_verbosity
         logger = logging.getLogger(name)
         logger.setLevel(self.log_levels[verbosity])
         return logger
 
-    # setting read-only attributes
     @property
     def config(self):
         return self._config
 
     @property
-    def save_dir(self):
-        return self._save_dir
+    def save_dirname(self):
+        return self._save_dirname
 
     @property
-    def log_dir(self):
-        return self._log_dir
+    def log_dirname(self):
+        return self._log_dirname
 
-# helper functions to update config dict with custom cli options
 def _update_config(config, modification):
     if modification is None:
         return config
@@ -143,15 +141,15 @@ def _update_config(config, modification):
 
 def _get_opt_name(flags):
     for flg in flags:
-        if flg.startswith('--'):
-            return flg.replace('--', '')
-    return flags[0].replace('--', '')
+        if flg.startswith("--"):
+            return flg.replace("--", "")
+    return flags[0].replace("--", "")
 
 def _set_by_path(tree, keys, value):
-    """Set a value in a nested object in tree by sequence of keys."""
-    keys = keys.split(';')
+    """ Set a value in a nested object in tree by sequence of keys."""
+    keys = keys.split(";")
     _get_by_path(tree, keys[:-1])[keys[-1]] = value
 
 def _get_by_path(tree, keys):
-    """Access a nested object in tree by sequence of keys."""
+    """ Access a nested object in tree by sequence of keys."""
     return reduce(getitem, keys, tree)
